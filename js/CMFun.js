9751e9194c8dadd2cdb8bff92d9fd718ccf22c80
xjshuang <xjshuang@Ctrip.com>
2015-08-07 15:34:12 +0800

    给予jQ的常用Fun的base搭建

---
+++ js/CMFun.js
@@ -0,0 +1,35 @@
+(function($, w) {
+	//sth comm Function
+	//copyright d.star
+    var _ = {};
+	if (!w.CMFun) w.CMFun = _;
+    //
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+})(jQuery, window);
